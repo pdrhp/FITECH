1f33294abafbf5c83d823edf376c61ae5583f884
Pedro Silva <pedrohsp2004@gmail.com>
2023-10-04 21:58:31 -0300

    Criação interface CalculoIMC

---
+++ app/src/main/java/com/pedrohucb/fitech/view/calculadoraimc/CalculadoraIMC.kt
@@ -0,0 +1,12 @@
+package com.pedrohucb.fitech.view.calculadoraimc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.pedrohucb.fitech.R
+
+class CalculadoraIMC : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calculadora_imc)
+    }
+}
