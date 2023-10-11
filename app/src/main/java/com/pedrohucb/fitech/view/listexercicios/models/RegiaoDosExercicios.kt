22a5895953f69870b7323feb4600833b76087a3b
Pedro Silva <pedrohsp2004@gmail.com>
2023-10-10 22:23:03 -0300

    Criação Modelo RegiaoDosExercicios e Enum TipoExercicio

---
+++ app/src/main/java/com/pedrohucb/fitech/view/listexercicios/models/RegiaoDosExercicios.kt
@@ -0,0 +1,6 @@
+package com.pedrohucb.fitech.view.listexercicios.models
+
+data class RegiaoDosExercicios(
+    val imagem : Int,
+    val tipoExercicio : TipoExercicio
+)
---
+++ app/src/main/java/com/pedrohucb/fitech/view/listexercicios/models/TipoExercicio.kt
@@ -0,0 +1,7 @@
+package com.pedrohucb.fitech.view.listexercicios.models
+
+enum class TipoExercicio(val descParte : String) {
+    REGIAOINFERIOR("Regiao inferior"),
+    REGIAOSUPERIOR("Região Superior"),
+    REGIAOABDOMINAL("Região Abdominal")
+}
