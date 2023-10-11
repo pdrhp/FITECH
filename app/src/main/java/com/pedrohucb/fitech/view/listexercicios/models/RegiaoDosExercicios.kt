f3d6c066d37342958773252d23e007bf03f556b7
Pedro Silva <pedrohsp2004@gmail.com>
2023-10-10 22:27:36 -0300

    Inserção Dados das Regiões dos exercicios

--- app/src/main/java/com/pedrohucb/fitech/view/listexercicios/models/RegiaoDosExercicios.kt
+++ app/src/main/java/com/pedrohucb/fitech/view/listexercicios/models/RegiaoDosExercicios.kt
@@ -1,6 +1,18 @@
 package com.pedrohucb.fitech.view.listexercicios.models
 
+import com.pedrohucb.fitech.R
+
 data class RegiaoDosExercicios(
     val imagem : Int,
     val tipoExercicio : TipoExercicio
 )
+
+object RegiaoDosExerciciosRepository{
+    fun getRegiaoDosExercicios(): List<RegiaoDosExercicios> {
+        return listOf(
+            RegiaoDosExercicios(R.drawable.regiao_superior, TipoExercicio.REGIAOSUPERIOR),
+            RegiaoDosExercicios(R.drawable.regiao_inferior, TipoExercicio.REGIAOINFERIOR),
+            RegiaoDosExercicios(R.drawable.regiao_abdominal, TipoExercicio.REGIAOABDOMINAL)
+        )
+    }
+}
