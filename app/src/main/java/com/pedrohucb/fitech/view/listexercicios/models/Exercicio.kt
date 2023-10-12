260417639bdba957fec1c21ab4f6859c74b0b412
Pedro Silva <pedrohsp2004@gmail.com>
2023-10-12 19:07:13 -0300

    Criação interface subList e inserção dados
    Criação da interface da activity da SubList, e inserção dos dados dos exercícios superiores

--- app/src/main/java/com/pedrohucb/fitech/view/listexercicios/models/Exercicio.kt
+++ app/src/main/java/com/pedrohucb/fitech/view/listexercicios/models/Exercicio.kt
@@ -11,7 +11,14 @@ data class Exercicio(
 
 object ExercicioRepositorySuperiores {
     fun getExercicios(): List<Exercicio> {
-        return listOf()
+        return listOf(
+            Exercicio(R.drawable.sup_full_pushups_img, "Flexão Completa", "Peitoral, triceps e ombro", "Dificuldade: Média"),
+            Exercicio(R.drawable.sup_wall_pushup_img, "Flexão na parede", "Peitoral, tricpes e ombro", "Dificuldade: Fácil"),
+            Exercicio(R.drawable.sup_knee_pushups_img, "Flexão de joelhos", "Peitoral, triceps e ombro", "Dificuldade: Fácil"),
+            Exercicio(R.drawable.sup_incline_pushups_img, "Flexão inclinada", "Peitoral, triceps e ombro", "Dificuldade: Fácil"),
+            Exercicio(R.drawable.sup_advanced_incline_pushups_img, "Flexão inclinada avançada", "Peitoral, triceps e ombro", "Dificuldade: Moderada"),
+            Exercicio(R.drawable.sup_narrow_pushups_img, "Flexão fechada", "Peitoral, triceps e ombro", "Dificuldade: Média/Dificil")
+        )
     }
 }
 
