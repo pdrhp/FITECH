86ad8c1ab9d00d296c6b1dd3ffc4144e9b4578f0
Pedro Silva <pedrohsp2004@gmail.com>
2023-10-13 14:18:05 -0300

    Fix Classe Exercicio

--- app/src/main/java/com/pedrohucb/fitech/view/listexercicios/ListExerciciosSubList.kt
+++ app/src/main/java/com/pedrohucb/fitech/view/listexercicios/ListExerciciosSubList.kt
@@ -19,20 +19,20 @@ class ListExerciciosSubList : AppCompatActivity() {
         binding = ActivityListExerciciosSubListBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val position = intent.getIntExtra("position", 3)
+        val regiao = intent.getIntExtra("position", 3)
 
-        if(position == 0){
+        if(regiao == 0){
         binding.listViewListaDeExercicios.adapter = AdapterExercicio(this, ExercicioRepositorySuperiores.getExercicios())
         }
-        else if(position == 1){
+        else if(regiao == 1){
             binding.listViewListaDeExercicios.adapter = AdapterExercicio(this, ExercicioRepositoryInferiores.getExercicios())
         }
-        else if(position == 2){
+        else if(regiao == 2){
             binding.listViewListaDeExercicios.adapter = AdapterExercicio(this, ExercicioRepositoryAbdominais.getExercicios())
         }
         
         binding.listViewListaDeExercicios.setOnItemClickListener { parent, view, position, id ->
-
+            DetalharExercicio(regiao, position)
         }
 
 
@@ -45,5 +45,12 @@ class ListExerciciosSubList : AppCompatActivity() {
         val intent = Intent(this, ListExercicios::class.java)
         startActivity(intent)
         finish()
+    }
+
+    private fun DetalharExercicio(regiao:Int, position : Int){
+        val intent = Intent(this, ExercicioDetailed::class.java)
+        intent.putExtra("regiao", regiao)
+        intent.putExtra("position", position)
+        startActivity(intent)
     }
 }
