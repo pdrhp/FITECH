6687ee45cd300dbf9ac2636e730ba42f4983b35c
Pedro Silva <pedrohsp2004@gmail.com>
2023-10-15 19:42:00 -0300

    fix activity sobreoapp

--- app/src/main/java/com/pedrohucb/fitech/view/sobreoapp/SobreOApp.kt
+++ app/src/main/java/com/pedrohucb/fitech/view/sobreoapp/SobreOApp.kt
@@ -4,9 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.pedrohucb.fitech.R
 
-class MainActivity : AppCompatActivity() {
+class SobreOApp : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_sobreoapp)
     }
 }
