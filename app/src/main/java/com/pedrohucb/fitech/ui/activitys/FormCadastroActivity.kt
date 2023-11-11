7bbf140cf9229b352dc78b6330dcb31af9221221
Pedro Silva <pedrohsp2004@gmail.com>
2023-11-11 16:27:57 -0300

    Add documentação FormCadastroActivity

--- app/src/main/java/com/pedrohucb/fitech/ui/activitys/FormCadastroActivity.kt
+++ app/src/main/java/com/pedrohucb/fitech/ui/activitys/FormCadastroActivity.kt
@@ -12,15 +12,25 @@ import com.google.firebase.auth.FirebaseAuthUserCollisionException
 import com.google.firebase.auth.FirebaseAuthWeakPasswordException
 import com.pedrohucb.fitech.databinding.ActivityFormCadastroBinding
 
+/**
+ * Activity para o formulário de cadastro.
+ */
 class FormCadastroActivity : AppCompatActivity() {
 
+    // Instância do objeto de binding com acesso às views do layout.
     private lateinit var binding: ActivityFormCadastroBinding
+    // Instância do FirebaseAuth para autenticação do usuário.
     private val auth = FirebaseAuth.getInstance()
+
+    /**
+     * Chamado quando a activity está iniciando.
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityFormCadastroBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Define o listener de clique para o botão de cadastro.
         binding.btCadastrarCadastro.setOnClickListener { view ->
 
             val email = binding.etInputEmailCadastro.text.toString();
@@ -31,6 +41,9 @@ class FormCadastroActivity : AppCompatActivity() {
 
     }
 
+    /**
+     * Cadastra o usuário.
+     */
     private fun CadastraUsuario(email: String, senha: String, view: View) {
         if (email.isEmpty() || senha.isEmpty()) {
             SnackBarErro(view, "Preencha todos os campos")
@@ -61,6 +74,9 @@ class FormCadastroActivity : AppCompatActivity() {
         }
     }
 
+    /**
+     * Exibe um Snackbar de aviso.
+     */
     private fun SnackBarAviso(view: View, mensagem: String) {
         val snackbar = Snackbar.make(
             view,
@@ -71,6 +87,9 @@ class FormCadastroActivity : AppCompatActivity() {
         snackbar.show()
     }
 
+    /**
+     * Exibe um Snackbar de sucesso.
+     */
     private fun snackBarSucesso(view: View, mensagem: String) {
         val snackbar = Snackbar.make(
             view,
@@ -81,6 +100,9 @@ class FormCadastroActivity : AppCompatActivity() {
         snackbar.show()
     }
 
+    /**
+     * Exibe um Snackbar de erro.
+     */
     private fun SnackBarErro(view: View, mensagem: String) {
         val snackbar =
             Snackbar.make(view, mensagem, Snackbar.LENGTH_SHORT)
