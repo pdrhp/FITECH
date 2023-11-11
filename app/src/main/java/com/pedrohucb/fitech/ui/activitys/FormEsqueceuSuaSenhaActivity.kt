75eafc01f9cb57a98d44178954662bea1f3806c8
Pedro Silva <pedrohsp2004@gmail.com>
2023-11-11 16:32:45 -0300

    Add documentação FormEsqueceuSuaSenhaActivity

--- app/src/main/java/com/pedrohucb/fitech/ui/activitys/FormEsqueceuSuaSenhaActivity.kt
+++ app/src/main/java/com/pedrohucb/fitech/ui/activitys/FormEsqueceuSuaSenhaActivity.kt
@@ -9,26 +9,38 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseAuthInvalidUserException
 import com.pedrohucb.fitech.databinding.ActivityFormEsqueceuSuaSenhaBinding
 
+/**
+ * Activity para o formulário de recuperação de senha.
+ */
 class FormEsqueceuSuaSenhaActivity : AppCompatActivity() {
 
+    // Instância do objeto de binding com acesso às views do layout.
     private lateinit var binding: ActivityFormEsqueceuSuaSenhaBinding
+
+    /**
+     * Chamado quando a activity está iniciando.
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityFormEsqueceuSuaSenhaBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
+        // Define o listener de clique para o botão de recuperação de senha.
         binding.btEsqueceuASenha.setOnClickListener {
             val email: String =
                 binding.etInputEmailEsqueceuASenha.text.toString().trim { it <= ' ' }
             MandaEmailDeRecuperacao(email, it)
         }
 
+        // Define o listener de clique para o botão de cancelar.
         binding.btCancelar.setOnClickListener {
             finish()
         }
     }
 
+    /**
+     * Envia o e-mail de recuperação de senha.
+     */
     private fun MandaEmailDeRecuperacao(email: String, it: View) {
         if (email.isEmpty()) {
             SnackBarErro(it, "Preencha o campo email")
@@ -48,12 +60,18 @@ class FormEsqueceuSuaSenhaActivity : AppCompatActivity() {
         }
     }
 
+    /**
+     * Exibe um Snackbar de erro.
+     */
     private fun SnackBarErro(it: View, mensagem: String) {
         val snack = Snackbar.make(it, mensagem, Snackbar.LENGTH_SHORT)
         snack.setBackgroundTint(Color.RED)
         snack.show()
     }
 
+    /**
+     * Exibe um Snackbar de sucesso.
+     */
     private fun SnackBarSucesso(it: View, mensagem: String) {
         val snack = Snackbar.make(it, mensagem, Snackbar.LENGTH_SHORT)
         snack.setBackgroundTint(Color.GREEN)
