c7e05158a32f2c61b29894818683b958de98f1d9
Pedro Silva <pedrohsp2004@gmail.com>
2023-11-11 13:11:45 -0300

    Add documentação CalculadoraIMCActivity

--- app/src/main/java/com/pedrohucb/fitech/ui/activitys/CalculadoraIMCActivity.kt
+++ app/src/main/java/com/pedrohucb/fitech/ui/activitys/CalculadoraIMCActivity.kt
@@ -15,29 +15,41 @@ import kotlinx.coroutines.withContext
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
+/**
+ * Activity para a Calculadora de IMC.
+ */
 class CalculadoraIMCActivity : AppCompatActivity() {
 
+    // Instância do objeto de binding com acesso às views do layout.
     private lateinit var binding: ActivityCalculadoraImcBinding
+    // ID do produto
     private var produtoId = 0L
 
+    // Inicialização via lazy do objeto DAO do banco de dados Room.
     private val calculosanterioresDao by lazy {
         val db = AppDatabase.instacia(this)
         db.calculosAnterioresDao()
     }
 
 
+    /**
+     * Chamado quando a activity está iniciando.
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCalculadoraImcBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
 
+        // Define o listener de clique para o botão de voltar.
         binding.iconButtonVoltarTelaIMC.setOnClickListener {
             Voltar()
         }
 
+        // Define o listener de clique para o botão de calcular.
         binding.buttonCalcularIMC.setOnClickListener {
 
+            // Verifica se os campos de peso e altura estão vazios.
             if (binding.inputPesoIMC.text.toString()
                     .isEmpty() || binding.inputAlturaIMC.text.isEmpty()
             ) {
@@ -47,7 +59,7 @@ class CalculadoraIMCActivity : AppCompatActivity() {
             val peso = binding.inputPesoIMC.text.toString()
             val altura = binding.inputAlturaIMC.text.toString()
 
-
+            // Verifica se o peso e a altura são válidos.
             if (peso.toFloat() <= 0 || altura.toFloat() <= 0) {
                 binding.textViewResultadoIMC.text = "Valores invalidos"
             } else if (peso.toFloat() > 0 && altura.toFloat() > 0) {
@@ -64,12 +76,15 @@ class CalculadoraIMCActivity : AppCompatActivity() {
 
             }
         }
-
+        // Define o listener de clique para o botão de histórico.
         binding.buttonCalculosAnteriores.setOnClickListener {
             NavegarHistorico();
         }
     }
 
+    /**
+     * Salva o cálculo no histórico.
+     */
     private fun salvaCalculoNoHistorico(novoCalculoAnterior: CalculoAnteriores) {
         lifecycleScope.launch {
             withContext(Dispatchers.IO) {
@@ -78,11 +93,17 @@ class CalculadoraIMCActivity : AppCompatActivity() {
         }
     }
 
+    /**
+     * Navega para a tela de histórico.
+     */
     private fun NavegarHistorico() {
         val intent = Intent(this, TelaHistoricoDeCalculosActivity::class.java)
         startActivity(intent)
     }
 
+    /**
+     * Calcula o IMC.
+     */
     private fun IMC(peso: String, altura: String): Float {
         val pesoFloat = peso.toFloat()
         val alturaFloat = altura.toFloat() / 100
@@ -92,13 +113,18 @@ class CalculadoraIMCActivity : AppCompatActivity() {
         return resultado;
     }
 
+    /**
+     * Volta para a tela Principal
+     */
     private fun Voltar() {
         val intent = Intent(this, TelaPrincipalActivity::class.java)
         startActivity(intent)
         finish()
     }
 
-
+    /**
+     * Cria um novo objeto do tipo CalculoAnteriores.
+     */
     private fun criaCalculosAnteriores(resultado: String, data: String): CalculoAnteriores {
         return CalculoAnteriores(
             id = produtoId,
