9bfc63f0af8d51bc4dab6e7557f4f5c8da9e3d35
Pedro Silva <pedrohsp2004@gmail.com>
2023-11-11 10:50:41 -0300

    Refatoração CalculadoraTMBActivity

--- app/src/main/java/com/pedrohucb/fitech/ui/activitys/CalculadoraTMBActivity.kt
+++ app/src/main/java/com/pedrohucb/fitech/ui/activitys/CalculadoraTMBActivity.kt
@@ -19,7 +19,7 @@ import okhttp3.Request
 
 class CalculadoraTMBActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityCalculadoratmbBinding
+    private lateinit var binding: ActivityCalculadoratmbBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,15 +27,18 @@ class CalculadoraTMBActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val genderItems = arrayOf("Masculino", "Feminino")
-        val atvFisItems = arrayOf("Sedentario", "Exercicito 1-3 semanalmente", "Exercito 4-5 Semanalmente", "Exercito Diaramente", "Exercito Intensamente todos os dias")
-
         val spinnerGenderAdapter = ArrayAdapter(this, R.layout.spinner_dropdown_item, genderItems)
+
+        val atvFisItems = arrayOf(
+            "Sedentario",
+            "Exercicito 1-3 semanalmente",
+            "Exercito 4-5 Semanalmente",
+            "Exercito Diaramente",
+            "Exercito Intensamente todos os dias"
+        )
         val spinnerAtvFisAdapter = ArrayAdapter(this, R.layout.spinner_dropdown_item, atvFisItems)
 
-        val applicationInfo : ApplicationInfo = application.packageManager.getApplicationInfo(application.packageName, PackageManager.GET_META_DATA)
-        val apiKey = applicationInfo.metaData["API_KEY"].toString()
-
-
+        val apiKey = recuperaAPIKEY()
 
         binding.inputGeneroTBM.adapter = spinnerGenderAdapter
         binding.inputNvAtvFisTBM.adapter = spinnerAtvFisAdapter
@@ -43,39 +46,60 @@ class CalculadoraTMBActivity : AppCompatActivity() {
         binding.iconButtonVoltarTelaTMB.setOnClickListener { VoltarTela() }
 
         binding.buttonCalcularTBM.setOnClickListener {
+            configuraBotaoCalcular(apiKey)
+        }
+    }
 
-            if(binding.inputIdadeTBM.text.toString().isEmpty() || binding.inputPesoTBM.text.toString().isEmpty() || binding.inputAlturaTBM.text.toString().isEmpty()){
-                binding.textViewResultadoTMB.text = "Informações invalidas"
-                return@setOnClickListener
-            }
-            val idade = binding.inputIdadeTBM.text.toString()
-            val peso = binding.inputPesoTBM.text.toString()
-            val altura = binding.inputAlturaTBM.text.toString()
-            val genero = HomemOuMulher(binding.inputGeneroTBM.selectedItem.toString())
-            val nvatvfis = NvAtvOptionSelector(binding.inputNvAtvFisTBM.selectedItem.toString())
+    private fun configuraBotaoCalcular(apiKey: String) {
+        if (binding.inputIdadeTBM.text.toString()
+                .isEmpty() || binding.inputPesoTBM.text.toString()
+                .isEmpty() || binding.inputAlturaTBM.text.toString().isEmpty()
+        ) {
+            binding.textViewResultadoTMB.text = "Informações invalidas"
+            return
+        }
+        val idade = binding.inputIdadeTBM.text.toString()
+        val peso = binding.inputPesoTBM.text.toString()
+        val altura = binding.inputAlturaTBM.text.toString()
+        val genero = HomemOuMulher(binding.inputGeneroTBM.selectedItem.toString())
+        val nvatvfis = NvAtvOptionSelector(binding.inputNvAtvFisTBM.selectedItem.toString())
 
-            CoroutineScope(Dispatchers.IO).launch {
-                val result = getCaloriasDiarias(idade, genero, altura, peso, nvatvfis, apiKey)
+        CoroutineScope(Dispatchers.IO).launch {
+            val result = getCaloriasDiarias(idade, genero, altura, peso, nvatvfis, apiKey)
 
-                withContext(Dispatchers.Main){
-                    if (result.data.BMR.isNotEmpty()){
-                        binding.textViewResultadoTMB.text = "${result.data.BMR} Calorias por dia"
-                    }
-                    else {
-                        binding.textViewResultadoTMB.text = "Resultado não encontrado"
-                    }
+            withContext(Dispatchers.Main) {
+                if (result.data.BMR.isNotEmpty()) {
+                    binding.textViewResultadoTMB.text = "${result.data.BMR} Calorias por dia"
+                } else {
+                    binding.textViewResultadoTMB.text = "Resultado não encontrado"
                 }
             }
         }
     }
 
-    private fun VoltarTela(){
+    private fun recuperaAPIKEY(): String {
+        val applicationInfo: ApplicationInfo = application.packageManager.getApplicationInfo(
+            application.packageName,
+            PackageManager.GET_META_DATA
+        )
+        val apiKey = applicationInfo.metaData["API_KEY"].toString()
+        return apiKey
+    }
+
+    private fun VoltarTela() {
         val intent = Intent(this, TelaPrincipalActivity::class.java)
         startActivity(intent)
         finish()
     }
 
-    private fun getCaloriasDiarias(idade:String, genero:String, altura:String, peso:String, nvAtvFis:String, apikey : String): DailyCaloriesRequest {
+    private fun getCaloriasDiarias(
+        idade: String,
+        genero: String,
+        altura: String,
+        peso: String,
+        nvAtvFis: String,
+        apikey: String
+    ): DailyCaloriesRequest {
         val client = OkHttpClient();
 
         val request = Request.Builder()
@@ -94,28 +118,24 @@ class CalculadoraTMBActivity : AppCompatActivity() {
         return bmr;
     }
 
-    private fun HomemOuMulher(genero: String): String{
-        if (genero == "Masculino"){
+    private fun HomemOuMulher(genero: String): String {
+        if (genero == "Masculino") {
             return "male"
-        }else{
+        } else {
             return "female"
         }
     }
 
-    private fun NvAtvOptionSelector(nivel : String): String{
-        if (nivel == "Sedentario"){
+    private fun NvAtvOptionSelector(nivel: String): String {
+        if (nivel == "Sedentario") {
             return "level_1"
-        }
-        else if(nivel == "Exercicito 1-3 semanalmente"){
+        } else if (nivel == "Exercicito 1-3 semanalmente") {
             return "level_2"
-        }
-        else if(nivel == "Exercito 4-5 Semanalmente"){
+        } else if (nivel == "Exercito 4-5 Semanalmente") {
             return "level_3"
-        }
-        else if(nivel == "Exercito Diaramente"){
+        } else if (nivel == "Exercito Diaramente") {
             return "level_4"
-        }
-        else if(nivel == "Exercito Intensamente todos os dias"){
+        } else if (nivel == "Exercito Intensamente todos os dias") {
             return "level_5"
         }
         return "Valor não encontrado"
