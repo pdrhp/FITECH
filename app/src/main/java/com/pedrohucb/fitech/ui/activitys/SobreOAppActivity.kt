43b7696f7060e46486f87749021c8f7ffaf41805
Pedro Silva <pedrohsp2004@gmail.com>
2023-11-11 18:31:16 -0300

    Add documentação SobreOAppActivity

--- app/src/main/java/com/pedrohucb/fitech/ui/activitys/SobreOAppActivity.kt
+++ app/src/main/java/com/pedrohucb/fitech/ui/activitys/SobreOAppActivity.kt
@@ -5,12 +5,21 @@ import com.pedrohucb.fitech.databinding.ActivitySobreoappBinding
 
 class SobreOAppActivity : AppCompatActivity() {
 
+    /**
+     * Activity para a tela "Sobre o App".
+     */
+    // Instância do objeto de binding com acesso às views do layout.
     private lateinit var binding : ActivitySobreoappBinding
+
+    /**
+     * Chamado quando a activity está iniciando.
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySobreoappBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Define o listener de clique para o botão de voltar.
         binding.iconButtonVoltarTelaSobreOApp.setOnClickListener {
             finish()
         }
